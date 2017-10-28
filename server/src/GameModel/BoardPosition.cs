3aa1e3711647ff8bc03d352f94cfb2fe25243de7
Zane Cersovsky <zcersovsky@gmail.com>
2017-10-28 18:24:53 -0500

    Added constructor for board position

--- server/src/GameModel/BoardPosition.cs
+++ server/src/GameModel/BoardPosition.cs
@@ -3,6 +3,12 @@ namespace GameModel {
     /// Represents a position on the board
     /// </summary>
     public struct BoardPosition {
+
+        public BoardPosition(XCoord x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
         public XCoord x;
         public int y;
     }
