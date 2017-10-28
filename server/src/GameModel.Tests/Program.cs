022558072a8c1c4f0e790df2e3ee760880f4c59c
Zane Cersovsky <zcersovsky@gmail.com>
2017-10-28 18:29:59 -0500

    Adding test project for GameModel

---
+++ server/src/GameModel.Tests/Program.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Common;
+using NUnit.Framework;
+using NUnitLite;
+using System;
+using System.Reflection;
+
+namespace GameModel.Tests
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            return new AutoRun(typeof(Program).GetTypeInfo().Assembly)
+                .Execute(args, new ExtendedTextWrapper(Console.Out), Console.In);
+        }
+    }
+}
