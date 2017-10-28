8fe52f6242cdd784c81375a833da2a01d90c1163
Taylor Walenczyk <tawalenczyk@ku.edu>
2017-10-27 21:21:01 -0500

    Built out simple camera rotation functionality

--- Chess^2/Assets/main_scene_camera_control.cs
+++ Chess^2/Assets/main_scene_camera_control.cs
@@ -21,4 +21,9 @@ public class main_scene_camera_control : MonoBehaviour {
 	void Update () {
 		
 	}
+
+	// on successful movement by a player, call this function to rotate the camera
+	void rotateNextPlayer() {
+		Camera.main.transform.Rotate(0, 0, 90); // rotate 90 degress about the z axis
+	}
 }
