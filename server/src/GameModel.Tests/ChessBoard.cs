26186674cb58233cdd6ce41ecae9c724c4711f23
Zane Cersovsky <zcersovsky@gmail.com>
2017-10-28 18:40:01 -0500

    Added tests for ChessBoard position bounds checking

---
+++ server/src/GameModel.Tests/ChessBoard.cs
@@ -0,0 +1,23 @@
+using NUnit.Framework;
+using GameModel;
+namespace GameModel.Tests
+{
+    [TestFixture]
+    class ChessBoardTests
+    {
+        [TestCase(XCoord.a, 0, false)] //Lower left corner
+        [TestCase(XCoord.n, 0, false)] //Lower right corner
+        [TestCase(XCoord.a, 13, false)] //Upper left corner
+        [TestCase(XCoord.n, 13, false)] //Upper right corner
+        [TestCase(XCoord.f, 6, true)] //Roughly middle
+        [TestCase(XCoord.a, 7, true)] //Left wing
+        [TestCase(XCoord.n, 7, true)] //Right wing
+        [TestCase(XCoord.f, 12, true)] //Top wing
+        [TestCase(XCoord.f, 02, true)] //Bottom wing
+        public void CheckPositionExists(XCoord x, int y, bool inBounds) {
+            var board = new ChessBoard();
+            var pos = new BoardPosition(x, y);
+            Assert.AreEqual(board.CheckPositionExists(pos), inBounds);
+        }
+    }
+}
