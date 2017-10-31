172417fd790bb455f2a7526d0a44c66d4d8bd528
Zane Cersovsky <zcersovsky@gmail.com>
2017-10-31 18:31:41 -0500

    Added PossibleMoves for the King

--- server/src/GameModel/Pieces/King.cs
+++ server/src/GameModel/Pieces/King.cs
@@ -5,13 +5,23 @@ namespace GameModel
 {
     class King : ChessPiece
     {
-        public override IEnumerable<BoardPosition> PossibleMoves(BoardPosition pos)
-        {
-                return Enumerable.Empty<BoardPosition>();
-        }
+        public override IEnumerable<BoardPosition> PossibleMoves(BoardPosition pos) =>
+            _moveOffsets.Select(posMove => new BoardPosition(pos.x + posMove.x, pos.y + posMove.y))
+                        .Where(candidate => ChessBoard.CheckPositionExists(candidate));
         public override bool CanMoveTo(BoardPosition position)
         {
             throw new System.NotImplementedException();
         }
+        private (int x, int y)[] _moveOffsets = 
+        {
+            (1, 1),
+            (1, 0),
+            (1, -1),
+            (0, -1),
+            (-1, -1),
+            (-1, 0),
+            (-1, 1),
+            (0, 1)
+        };
     }
 }
