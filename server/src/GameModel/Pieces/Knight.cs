6949f5894d78b8039fd3b53a645af201c463aece
Zane Cersovsky <zcersovsky@gmail.com>
2017-10-31 18:35:09 -0500

    Added PossibleMoves for the knight

--- server/src/GameModel/Pieces/Knight.cs
+++ server/src/GameModel/Pieces/Knight.cs
@@ -5,13 +5,23 @@ namespace GameModel
 {
     class Knight : ChessPiece
     {
-        public override IEnumerable<BoardPosition> PossibleMoves(BoardPosition pos)
-        {
-            return Enumerable.Empty<BoardPosition>();
-        }
+        public override IEnumerable<BoardPosition> PossibleMoves(BoardPosition pos) =>
+            _moveOffsets.Select(posMove => new BoardPosition(pos.x + posMove.x, pos.y + posMove.y))
+                        .Where(candidate => ChessBoard.CheckPositionExists(candidate));
         public override bool CanMoveTo(BoardPosition position)
         {
             throw new System.NotImplementedException();
         }
+        private (int x, int y)[] _moveOffsets = 
+        {
+            (1, 2),
+            (2, 1),
+            (2, -1),
+            (1, -2),
+            (-1, -2),
+            (-2, -1),
+            (-2, 1),
+            (-1, 2)
+        };
     }
 }
