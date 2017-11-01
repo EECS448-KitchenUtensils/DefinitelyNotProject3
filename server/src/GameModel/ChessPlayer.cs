e8741b9e8413799dccd24f7641f7b6451806ba62
Zane Cersovsky <zcersovsky@gmail.com>
2017-10-31 21:03:58 -0500

    Added a constructor to ChessPlayer

--- server/src/GameModel/ChessPlayer.cs
+++ server/src/GameModel/ChessPlayer.cs
@@ -4,6 +4,10 @@ namespace GameModel
     /// Represents an individual player
     /// </summary>
     public class ChessPlayer {
+        public ChessPlayer(PlayerEnum me)
+        {
+            Player = me;
+        }
         /// <summary>
         /// Which player slot this is (1..4)
         /// </summary>
