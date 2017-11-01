4cdac42a0a8afad274c01054ef27e01729d65295
Taylor Walenczyk <tawalenczyk@ku.edu>
2017-10-31 20:33:15 -0500

    Possible moves defined for Rook

--- server/src/GameModel/Pieces/Rook.cs
+++ server/src/GameModel/Pieces/Rook.cs
@@ -7,11 +7,34 @@ namespace GameModel
     {
         public override IEnumerable<BoardPosition> PossibleMoves(BoardPosition pos)
         {
-            return Enumerable.Empty<BoardPosition>();
+            for(int i = 0; i < 4; i++) {
+
+                // iterate until no board position exists
+                for(int centerDistance = 1; true; centerDistance++)
+                {
+                    var newX = pos.x + (_moveOffsets[i].x * centerDistance);
+                    var newY = pos.y + (_moveOffseets[i].y * centerDistance);
+                    var candidate = new BoardPosition(newX, newY);
+
+                    if (ChessBoard.CheckPositionExists(candidate))
+                        yield return candidate;
+                    else
+                        break;
+                }
+            }
         }
+
         public override bool CanMoveTo(BoardPosition position)
         {
             throw new System.NotImplementedException();
         }
+
+        private (int x, int y)[] _moveOffsets =
+        {
+            (1, 0),
+            (0, -1),
+            (-1, 0),
+            (0, 1)
+        };
     }
 }
