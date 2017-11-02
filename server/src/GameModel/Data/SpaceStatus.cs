4fc4fc3789901f61138332f51c26401a3659e7d7
Zane Cersovsky <zcersovsky@gmail.com>
2017-11-02 18:42:04 -0500

    Added an enum to represent the result of a position check

---
+++ server/src/GameModel/Data/SpaceStatus.cs
@@ -0,0 +1,13 @@
+﻿namespace GameModel.Data
+{
+    /// <summary>
+    /// Represents whether a position is empty, occupied by an enemy, or occupied by 
+    /// a friendly piece
+    /// </summary>
+    enum SpaceStatus
+    {
+        Empty,
+        Friendly,
+        Enemy
+    }
+}
