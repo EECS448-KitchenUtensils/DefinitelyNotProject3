c4f50a1de8bf269cfcb739137c2ab8400e693d8d
Zane Cersovsky <zcersovsky@gmail.com>
2017-11-02 20:48:06 -0500

    I love C#. I swear.

--- server/src/GameModel/Data/BoardPosition.cs
+++ server/src/GameModel/Data/BoardPosition.cs
@@ -9,6 +9,24 @@ namespace GameModel {
             this.x = x;
             this.y = y;
         }
+        public static bool operator == (BoardPosition first, BoardPosition second)
+        {
+            return first.x == second.x && first.y == second.y;
+        }
+        public static bool operator != (BoardPosition first, BoardPosition second)
+        {
+            return !(first == second);
+        }
+        public override bool Equals(object obj)
+        {
+            switch (obj)
+            {
+                case BoardPosition other:
+                    return other.x == x && other.y == y;
+                default:
+                    return false;
+            }
+        }
         public override int GetHashCode() => (int)x ^ y;
         public XCoord x;
         public int y;
