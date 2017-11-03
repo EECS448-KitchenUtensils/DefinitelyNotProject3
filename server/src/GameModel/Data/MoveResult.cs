a5e172e4b87286b8c90e6e049a45cd76af224df1
Zane Cersovsky <zcersovsky@gmail.com>
2017-11-03 17:28:15 -0500

    Added MoveResult to replace a usage of ValueTuple

---
+++ server/src/GameModel/Data/MoveResult.cs
@@ -0,0 +1,26 @@
+﻿namespace GameModel.Data
+{
+    public struct MoveResult
+    {
+        /// <summary>
+        /// Creates new MoveResult
+        /// </summary>
+        /// <param name="pos">The destination position of this move</param>
+        /// <param name="outcome">The outcome of this move</param>
+        public MoveResult(BoardPosition pos, MoveType outcome)
+        {
+            Position = pos;
+            Outcome = outcome;
+        }
+        
+        /// <summary>
+        /// The destination position of this move
+        /// </summary>
+        public BoardPosition Position { get; }
+
+        /// <summary>
+        /// The outcome of this move
+        /// </summary>
+        public MoveType Outcome { get; }
+    }
+}
