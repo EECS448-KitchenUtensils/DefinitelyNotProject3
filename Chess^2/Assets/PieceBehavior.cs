4d0747fc46b41308deb599302e91ec257b71036c
Jack Weber <jackweber@ku.edu>
2017-11-02 21:29:28 -0500

    Basic starting logic for piece movement.

--- Chess^2/Assets/PieceBehavior.cs
+++ Chess^2/Assets/PieceBehavior.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PieceBehavior : MonoBehaviour {
 
 	public string thisPiece;
+	private GameObject tempSquare;
 
 	// Use this for initialization
 	void Start () {
@@ -13,11 +14,21 @@ public class PieceBehavior : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(Input.GetMouseButtonUp(0)){
+			Destroy (tempSquare);
+		}
 	}
 
 	void OnMouseDown (){
+
+		GameObject square = GameObject.Find("sqaure_dark");
+
 		if (thisPiece.Contains ("pawn")) {
+			float x = this.gameObject.transform.position.x;
+			float y = this.gameObject.transform.position.y;
+			y += 2;
+			tempSquare = Instantiate (square, new Vector3 (x, y, -0.1f), Quaternion.identity);
+			tempSquare.GetComponent<Renderer> ().material.color = new Color (0, 0, 1);
 
 		} else if (thisPiece.Contains ("king")) {
 
