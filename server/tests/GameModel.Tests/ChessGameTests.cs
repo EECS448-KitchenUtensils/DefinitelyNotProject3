f9b53a206916601a38b2dd7fde23be14b0f3aa1b
Zane Cersovsky <zcersovsky@gmail.com>
2017-11-04 17:01:24 -0500

    Added tests for MakeMove's bounds checking behavior

--- server/tests/GameModel.Tests/ChessGameTests.cs
+++ server/tests/GameModel.Tests/ChessGameTests.cs
@@ -1,8 +1,13 @@
-﻿namespace GameModel.Tests
+﻿using GameModel.Data;
+using NUnit.Framework;
+using System.Collections;
+using static GameModel.Tests.TestUtils;
+
+namespace GameModel.Tests
 {
+    [TestFixture]
     class ChessGameTests
     {
-        //Test MakeMove for src & dest bound checks
         //Test MakeMove works for valid moves
         //Test MakeMove fails for invalid moves
         //Test MakeMove honors which player is active
@@ -10,5 +15,27 @@
         //Test MakeMove loops back to Player 1 after Player 4 is done
         //Test GetPieceByPosition works for valid pieces
         //Test GetPieceByPosition returns null for invalid pieces
+        [SetUp]
+        public void Init()
+        {
+            _chessGame = new ChessGame();
+        }
+        [Test, TestCaseSource("BoundsCheckCases")]
+        public void MakeMoveBoundChecks(BoardPosition src, BoardPosition dest)
+        {
+            var result = _chessGame.MakeMove(src, dest);
+            Assert.That(result, Is.EqualTo(MoveType.Failure));
+        }
+        public static IEnumerable BoundsCheckCases
+        {
+            get
+            {
+                yield return new TestCaseData(Pos(XCoord.a, 1), Pos(XCoord.a, 2))
+                    .SetName("MakeMoveBoundsChecksInvalidSrcValidDest");
+                yield return new TestCaseData(Pos(XCoord.a, 4), Pos(XCoord.a, 3))
+                    .SetName("MakeMoveBoundsChecksValidSrcInvalidDest");
+            }
+        }
+        private ChessGame _chessGame;
     }
 }
