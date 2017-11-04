3002edbedaeef0ca01ab2868c9e7eadff32f20c3
Zane Cersovsky <zcersovsky@gmail.com>
2017-11-04 16:59:55 -0500

    Moved common test methods out into their own file

---
+++ server/tests/GameModel.Tests/TestUtils.cs
@@ -0,0 +1,10 @@
+﻿using GameModel.Data;
+
+namespace GameModel.Tests
+{
+    public static class TestUtils
+    {
+        public static BoardPosition Pos(XCoord x, int y) => new BoardPosition(x, y);
+        public static MoveResult Move(XCoord x, int y, MoveType result) => new MoveResult(Pos(x, y), result);
+    }
+}
