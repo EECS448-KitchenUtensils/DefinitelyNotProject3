063ee5d845591fc6bef7000176124aba2b7943df
Zane Cersovsky <zcersovsky@gmail.com>
2017-11-04 16:28:57 -0500

    Added todo list for ChessGameTests as a comment

---
+++ server/tests/GameModel.Tests/ChessGameTests.cs
@@ -0,0 +1,14 @@
+﻿namespace GameModel.Tests
+{
+    class ChessGameTests
+    {
+        //Test MakeMove for src & dest bound checks
+        //Test MakeMove works for valid moves
+        //Test MakeMove fails for invalid moves
+        //Test MakeMove honors which player is active
+        //Test MakeMove increments the active player count on success
+        //Test MakeMove loops back to Player 1 after Player 4 is done
+        //Test GetPieceByPosition works for valid pieces
+        //Test GetPieceByPosition returns null for invalid pieces
+    }
+}
