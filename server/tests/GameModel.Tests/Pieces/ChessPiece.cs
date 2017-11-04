774cfcb8493d66bc5d3beb9f9bbdcd81f15d8e22
Zane Cersovsky <zcersovsky@gmail.com>
2017-11-04 15:03:08 -0500

    Reworked the ChessPiece test framework

--- server/tests/GameModel.Tests/Pieces/ChessPiece.cs
+++ server/tests/GameModel.Tests/Pieces/ChessPiece.cs
@@ -8,11 +8,10 @@ namespace GameModel.Tests.Pieces
     class ChessPieceTests
     {
         /// <summary>
-        /// Checks what a piece does on an empty board
+        /// Returns a positionChecker that assumes the board is completely empty
         /// </summary>
-        /// <param name="piece"></param>
-        /// <param name="expectedMoves"></param>
-        protected void PossibleMovesReturnsOnlyValidMovesInEmptySpace(ChessPiece piece, ISet<MoveResult> expectedMoves)
+        /// <returns></returns>
+        protected static Func<BoardPosition, SpaceStatus> EmptyChecker()
         {
             SpaceStatus positionChecker(BoardPosition pos)
             {
@@ -21,17 +20,24 @@ namespace GameModel.Tests.Pieces
                 else
                     return SpaceStatus.Void;
             }
-            PossibleMovesReturnsOnlyValidMoves(piece, expectedMoves, positionChecker);
+            return positionChecker;
         }
 
-        protected void PossibleMovesInABox(ChessPiece piece, int boxRadius, SpaceStatus boxType, ISet<MoveResult> expectedMoves)
+        /// <summary>
+        /// Returns a positionChecker that simulates the piece surround by a ring of other pieces
+        /// </summary>
+        /// <param name="piecePosition">The position of the piece under test</param>
+        /// <param name="boxRadius">How many spaces between the ring and the piece under test</param>
+        /// <param name="boxType">What type of pieces the ring is composed of</param>
+        /// <returns></returns>
+        protected static Func<BoardPosition, SpaceStatus> BoxChecker(BoardPosition piecePosition, int boxRadius, SpaceStatus boxType = SpaceStatus.Friendly)
         {
             SpaceStatus positionChecker(BoardPosition pos)
             {
                 if (ChessBoard.CheckPositionExists(pos))
                 {
-                    var xDistance = Math.Abs(piece.Position.X - pos.X);
-                    var yDistance = Math.Abs(piece.Position.Y - pos.Y);
+                    var xDistance = Math.Abs(piecePosition.X - pos.X);
+                    var yDistance = Math.Abs(piecePosition.Y - pos.Y);
                     if (xDistance > boxRadius || yDistance > boxRadius)
                         return boxType;
                     else
@@ -40,8 +46,9 @@ namespace GameModel.Tests.Pieces
                 else
                     return SpaceStatus.Void;
             }
-            PossibleMovesReturnsOnlyValidMoves(piece, expectedMoves, positionChecker);
+            return positionChecker;
         }
+
 
         /// <summary>
         /// Helper method that allows the tests to generalized
@@ -49,7 +56,7 @@ namespace GameModel.Tests.Pieces
         /// <param name="piece">A piece instance at some position</param>
         /// <param name="expectedMoves"></param>
         /// <param name="positionChecker"></param>
-        private void PossibleMovesReturnsOnlyValidMoves(ChessPiece piece, ISet<MoveResult> expectedMoves, Func<BoardPosition, SpaceStatus> positionChecker)
+        protected void PossibleMovesReturnsOnlyValidMoves(ChessPiece piece, ISet<MoveResult> expectedMoves, Func<BoardPosition, SpaceStatus> positionChecker)
         {
             var calculatedMoves = piece.PossibleMoves(positionChecker);
             Assert.That(() => expectedMoves.SetEquals(calculatedMoves), Is.True);
