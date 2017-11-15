407850ab7dbed86298e0b54d85e5ccb2310f4d8d
jackdweber <jackdweber@gmail.com>
2017-11-14 21:07:28 -0600

    Added script to join button.

---
+++ CHESSx4/Assets/ui/JoinButtonBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoinButtonBehaviour : MonoBehaviour
+{
+
+    //Switch Scene
+    public void GoToMainScene()
+    {
+        Debug.Log("You have requested to go to the main scene");
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
