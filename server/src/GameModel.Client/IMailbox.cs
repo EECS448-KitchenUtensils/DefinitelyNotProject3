88a758b6b9aa5c8019691b7a2be5397be80d3fe5
Zane Cersovsky <zcersovsky@gmail.com>
2017-11-19 15:01:49 -0800

    Added base classes for message-passing client implementation

---
+++ server/src/GameModel.Client/IMailbox.cs
@@ -0,0 +1,24 @@
+﻿using GameModel.Messages;
+
+namespace GameModel.Client
+{
+    public interface IMailbox
+    {
+        /// <summary>
+        /// Puts a message into the send queue
+        /// </summary>
+        /// <param name="msg">Message to send</param>
+        void SendMessage(ModelMessage msg);
+
+        /// <summary>
+        /// Attempts to retrieve a message from the queue
+        /// </summary>
+        /// <returns>A retrieved ModelMessage or null</returns>
+        ModelMessage RetrieveMessage();
+
+        /// <summary>
+        /// Checks if the receive queue has messages in it
+        /// </summary>
+        bool HasMessages { get; }
+    }
+}
