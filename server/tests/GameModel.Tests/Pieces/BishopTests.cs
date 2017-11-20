58c9e30dfd1a2357f45efc9f94de337d4b619ef9
Zane Cersovsky <zcersovsky@gmail.com>
2017-11-19 20:12:29 -0600

    Fixed and included BishopTests

--- server/tests/GameModel.Tests/Pieces/BishopTests.cs
+++ server/tests/GameModel.Tests/Pieces/BishopTests.cs
@@ -11,7 +11,8 @@ namespace GameModel.Tests.Pieces
         [Test, TestCaseSource("PossibleMoveCases")]
         public void PossibleMoves(BoardPosition initialPosition, IEnumerable<MoveResult> validMoves, Func<BoardPosition, SpaceStatus> positionChecker)
         {
-            var bishop = new Bishop(PlayerEnum.PLAYER_1, Pos(XCoord.f, 5));
+            var player1 = new Player(PlayerEnum.PLAYER_1);
+            var bishop = new Bishop(player1, Pos(XCoord.f, 5));
             PossibleMovesReturnsOnlyValidMoves(bishop, validMoves, positionChecker);
         }
 
@@ -26,23 +27,17 @@ namespace GameModel.Tests.Pieces
                     Move(XCoord.e, 6, MoveType.Move),
                     Move(XCoord.d, 7, MoveType.Move),
                     Move(XCoord.c, 8, MoveType.Move),
-                    Move(XCoord.b, 9, MoveType.Move),
-                    Move(XCoord.a, 10, MoveType.Move),
                     // upper-right diagonal
                     Move(XCoord.g, 6, MoveType.Move),
                     Move(XCoord.h, 7, MoveType.Move),
-                    Move(XCoord.i, 8, MoveType.Move),
-                    Move(XCoord.j, 9, MoveType.Move),
-                    Move(XCoord.k, 10, MoveType.Move),
-                    Move(XCoord.l, 11, MoveType.Move),
                     // lower-right diagonal
                     Move(XCoord.g, 4, MoveType.Move),
                     Move(XCoord.h, 3, MoveType.Move),
-                    Move(XCoord.i, 2, MoveType.Move),
-                    Move(XCoord.j, 1, MoveType.Move),
                     // lower-left diagonal
                     Move(XCoord.e, 4, MoveType.Move),
-                    Move(XCoord.d, 3, MoveType.Move)
+                    Move(XCoord.d, 3, MoveType.Move),
+                    Move(XCoord.c, 2, MoveType.Move),
+                    Move(XCoord.b, 1, MoveType.Move)
                 }, EmptyChecker()
                 ).SetName("BishopPossibleMovesEmptySpace");
 
