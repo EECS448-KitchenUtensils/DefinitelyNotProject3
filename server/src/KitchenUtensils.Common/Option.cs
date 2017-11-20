9cd53dc0f66560d6b0b0a41a9f140d060017bf0f
Zane Cersovsky <zcersovsky@gmail.com>
2017-11-19 20:43:57 -0600

    Added a Common project and an Option type

---
+++ server/src/KitchenUtensils.Common/Option.cs
@@ -0,0 +1,24 @@
+﻿namespace KitchenUtensils.Common
+{
+    public abstract class Option<T>
+    {
+        public static Option<T> Fulfill(T value) => new Fulfilled<T>(value);
+        public static Reject<T> Reject() => new Reject<T>();
+    }
+
+    public class Reject<T>
+    {
+        public Reject()
+        {
+        }
+    }
+
+    public class Fulfilled<T> : Option<T>
+    {
+        public Fulfilled(T value)
+        {
+            Value = value;
+        }
+        public T Value { get; }
+    }
+}
