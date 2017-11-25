334932865cedb46e27a9b20c7ee6ee27c52812cd
Zane Cersovsky <zcersovsky@gmail.com>
2017-11-25 10:35:08 -0800

    Added comments, removed unneeded equality operators, and added an InGame property to Player class

--- server/src/GameModel/Data/Player.cs
+++ server/src/GameModel/Data/Player.cs
@@ -1,32 +1,31 @@
-﻿using System;
-namespace GameModel.Data
+﻿namespace GameModel.Data
 {
     public class Player
     {
+        /// <summary>
+        /// Creates a <see cref="Player"/> instance
+        /// </summary>
+        /// <param name="order">The order (1st, 2nd, etc.) that they player will take</param>
         public Player(PlayerEnum order)
         {
             Precedence = order;
             Checked = false;
+            InGame = true;
         }
-
-        public static bool operator ==(Player first, Player second) => first.Precedence == second.Precedence;
-
-        public static bool operator !=(Player first, Player second) => !(first == second);
-
-        public override bool Equals(object obj)
-        {
-            switch (obj)
-            {
-                case Player other:
-                    return other.Precedence == Precedence;
-                default:
-                    return false;
-            }
-        }
-
-        public override int GetHashCode() => (int)Precedence;
-
+        
+        /// <summary>
+        /// The player's order (1, 2, etc.)
+        /// </summary>
         public PlayerEnum Precedence { get; }
+
+        /// <summary>
+        /// Is this player in check?
+        /// </summary>
         public bool Checked { get; internal set; }
+
+        /// <summary>
+        /// Is this player still in the game?
+        /// </summary>
+        public bool InGame { get; internal set; }
     }
 }
