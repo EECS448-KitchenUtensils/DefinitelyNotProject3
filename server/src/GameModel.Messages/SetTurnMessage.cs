6cb3845685d0cc3bdf093bc8378dd4f41cc73edf
Zane Cersovsky <zcersovsky@gmail.com>
2017-11-25 12:49:27 -0800

    Added SetTurnMessage

---
+++ server/src/GameModel.Messages/SetTurnMessage.cs
@@ -0,0 +1,26 @@
+﻿using GameModel.Data;
+using System;
+
+namespace GameModel.Messages
+{
+    /// <summary>
+    /// Emitted when a turn is advanced
+    /// </summary>
+    [Serializable]
+    public class SetTurnMessage: ModelMessage
+    {
+        /// <summary>
+        /// Creates a <see cref="SetTurnMessage"/>
+        /// </summary>
+        /// <param name="player">The <see cref="Player"/> that will own this turn</param>
+        public SetTurnMessage(Player player)
+        {
+            this.player = player.Precedence;
+        }
+
+        /// <summary>
+        /// The player that owns the new turn
+        /// </summary>
+        public readonly PlayerEnum player;
+    }
+}
