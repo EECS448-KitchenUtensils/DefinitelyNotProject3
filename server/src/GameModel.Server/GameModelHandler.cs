9bafa422b2b834c00ae35ed4b8aa1c52a26359b0
Zane Cersovsky <zcersovsky@gmail.com>
2017-11-25 19:04:23 -0600

    Initial project import for server

---
+++ server/src/GameModel.Server/GameModelHandler.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Web.WebSockets;
+
+namespace GameModel.Server
+{
+    /// <summary>
+    /// Handles a connection with a client GameModel over a websocket
+    /// </summary>
+    public class GameModelHandler: WebSocketHandler
+    {
+        public override void OnOpen()
+        {
+            base.OnOpen();
+        }
+
+        public override void OnMessage(string message)
+        {
+            base.OnMessage(message);
+        }
+
+        public override void OnClose()
+        {
+            base.OnClose();
+        }
+    }
+}
---
+++ server/src/GameModel.Server/sock.ashx
@@ -0,0 +1,1 @@
+﻿<%@ WebHandler Language="C#" CodeBehind="sock.ashx.cs" Class="GameModel.Server.WebSocketHttpHandler" %>
