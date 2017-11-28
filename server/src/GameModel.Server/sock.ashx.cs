2c9c7a42f256b4a8139ae7b7b7430e31b808604d
Zane Cersovsky <zcersovsky@gmail.com>
2017-11-28 09:17:31 -0600

    Fixed the websocket controller

--- server/src/GameModel.Server/sock.ashx.cs
+++ server/src/GameModel.Server/sock.ashx.cs
@@ -10,19 +10,27 @@ namespace GameModel.Server
     /// </summary>
     public class WebSocketHttpHandler : IHttpHandler
     {
-
+        /// <summary>
+        /// Processes an HTTP request
+        /// </summary>
+        /// <param name="context"></param>
         public void ProcessRequest(HttpContext context)
         {
             if (context.IsWebSocketRequest)
             {
-                context.AcceptWebSocketRequest(new GameModelHandler());
+                context.AcceptWebSocketRequest(new GameModelHandler().ProcessWebSocketRequestAsync);
             }
             else
             {
-
+                context.Response.StatusCode = 400;
+                context.Response.Write("Expected a Websocket Upgrade\r\n");
+                context.Response.Close();
             }
         }
 
+        /// <summary>
+        /// This Controller is reusable between requests
+        /// </summary>
         public bool IsReusable
         {
             get
